b1d4da98c0cd5437e3cf9f52affc3ec5558c2ee2
wlt <1206506751@qq.com>
2022-02-18 16:23:33 +0800

    fix: traceback example data add url

--- final_django/views/example.py
+++ final_django/views/example.py
@@ -9,11 +9,13 @@ EXAMPLE_DATA = [
         "traceback": [
             {
                 "text": "双车道施工便道宽度不宜小于6.5m",
-                "source": "公路工程施工安全技术规范 JTGF90-2015"
+                "source": "公路工程施工安全技术规范 JTGF90-2015",
+                "url": "http://www.jianbiaoku.com/webarbs/book/72252/1505250.shtml"
             },
             {
                 "text": "单车道施工便道宽度不宜小于4.5m，并宜设置错车道，错车道应设置在视野良好的帝端，间距不宜大于300m",
-                "source": "公路工程施工安全技术规范 JTGF90-2015"
+                "source": "公路工程施工安全技术规范 JTGF90-2015",
+                "url": "http://www.jianbiaoku.com/webarbs/book/72252/1505250.shtml"
             }
         ]
     },
@@ -23,7 +25,8 @@ EXAMPLE_DATA = [
         "traceback": [
             {
                 "text": "配电柜正面的操作通道宽度，单列布置或双列背对背布置不小于1.5m，双列面对面布置不小于2m",
-                "source": "施工现场临时用电安全技术规范 [附条文说明] JGJ46-2005"
+                "source": "施工现场临时用电安全技术规范 [附条文说明] JGJ46-2005",
+                "url": "http://www.jianbiaoku.com/webarbs/book/194/1012888.shtml"
             }
         ]
     },
@@ -33,11 +36,13 @@ EXAMPLE_DATA = [
         "traceback": [
             {
                 "text": "手提式灭火器宜设置在灭火器箱内或挂钩、托架上",
-                "source": " 建筑灭火器配置验收及检查规范 [附条文说明] GB50444-2008"
+                "source": "建筑灭火器配置验收及检查规范 [附条文说明] GB50444-2008",
+                "url": "http://www.jianbiaoku.com/webarbs/book/12313/694922.shtml"
             },
             {
                 "text": "嵌墙式灭火器箱及挂钩、托架的安装高度应满足手提式灭火器顶部离地面距离不大于1.50m，底部离地面距离不小于0.08m",
-                "source": "建筑灭火器配置验收及检查规范 [附条文说明] GB50444-2008"
+                "source": "建筑灭火器配置验收及检查规范 [附条文说明] GB50444-2008",
+                "url": "http://www.jianbiaoku.com/webarbs/book/12313/694922.shtml"
             }
         ]
     },
@@ -47,7 +52,8 @@ EXAMPLE_DATA = [
         "traceback": [
             {
                 "text": "塔身高于30m的塔式起重机，应在塔顶和臂架端部设红色信号灯",
-                "source": "施工现场临时用电安全技术规范 [附条文说明] JGJ46-2005"
+                "source": "施工现场临时用电安全技术规范 [附条文说明] JGJ46-2005",
+                "url": "http://www.jianbiaoku.com/webarbs/book/194/1012888.shtml"
             }
         ]
     },
@@ -57,11 +63,13 @@ EXAMPLE_DATA = [
         "traceback": [
             {
                 "text": "交流电焊机一次线长度不得超过5m，应穿管保护",
-                "source": "施工现场机械设备检查技术规范 [附条文说明] JGJ160-2016"
+                "source": "施工现场机械设备检查技术规范 [附条文说明] JGJ160-2016",
+                "url": "http://www.jianbiaoku.com/webarbs/book/10446/2830395.shtml"
             },
             {
                 "text": "电焊机的二次线应采用防水橡皮护套铜芯软电缆，电缆长度不宜大于30m，一次线长度不宜大于5m",
-                "source": "施工现场机械设备检查技术规范 [附条文说明] JGJ160-2016"
+                "source": "施工现场机械设备检查技术规范 [附条文说明] JGJ160-2016",
+                "url": "http://www.jianbiaoku.com/webarbs/book/10446/2830395.shtml"
             }
         ]
     },
@@ -71,7 +79,8 @@ EXAMPLE_DATA = [
         "traceback": [
             {
                 "text": "每道剪刀撑宽度不应小于4跨，且不应小于6m，斜杆与地面的倾角应在45°～60°之间",
-                "source": "建筑施工扣件式钢管脚手架安全技术规范 [附条文说明] JGJ130-2011"
+                "source": "建筑施工扣件式钢管脚手架安全技术规范 [附条文说明] JGJ130-2011",
+                "url": "http://www.jianbiaoku.com/webarbs/book/10313/2733463.shtml"
             }
         ]
     },
@@ -81,7 +90,8 @@ EXAMPLE_DATA = [
         "traceback": [
             {
                 "text": "长度小于300m的隧道，起爆站应设在洞口侧面50m以外；其余隧道洞内起爆站距爆破位置不得小于300m",
-                "source": "公路工程施工安全技术规范 JTGF90-2015"
+                "source": "公路工程施工安全技术规范 JTGF90-2015",
+                "url": "http://www.jianbiaoku.com/webarbs/book/72252/1505250.shtml"
             }
         ]
     },
@@ -91,11 +101,13 @@ EXAMPLE_DATA = [
         "traceback": [
             {
                 "text": "高处作业 ：在坠落高度基准面2m及以上有可能坠落的高处进行的作业",
-                "source": "建筑施工高处作业安全技术规范 [附条文说明] JGJ80-2016"
+                "source": "建筑施工高处作业安全技术规范 [附条文说明] JGJ80-2016",
+                "url": "http://www.jianbiaoku.com/webarbs/book/10791/2633450.shtml"
             },
             {
                 "text": "悬空高处作业人员应挂牢安全带",
-                "source": "建筑施工安全技术统一规范[附条文说明]GB 50870-2013"
+                "source": "建筑施工安全技术统一规范[附条文说明]GB 50870-2013",
+                "url": "http://www.jianbiaoku.com/webarbs/book/10791/2633450.shtml"
             }
         ]
     }
